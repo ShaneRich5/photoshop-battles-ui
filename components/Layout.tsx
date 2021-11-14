221d29e0b32f95d4645c68c204304ce2bcffeec3
Shane <shane.richards121@gmail.com>
2021-11-14 00:01:33 -0500

    chore: add meta tags and assets

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,8 +6,17 @@ const Layout: React.FC = ({ children }) => {
     <React.Fragment>
       <Head>
         <title key="title">Photoshop Battles</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" key="apple-touch-icon"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" key="icon-32"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" key="icon-16"/>
+        <link rel="manifest" href="/site.webmanifest" key="manifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" key="mask-icon"/>
+        <meta name="msapplication-TileColor" content="#da532c" key="ms-tile-color"/>
+        <meta name="theme-color" content="#ffffff" key="theme-color"/>
       </Head>
-      {children}
+      <div className="pb-6">
+        {children}
+      </div>
     </React.Fragment>
   )
 }
